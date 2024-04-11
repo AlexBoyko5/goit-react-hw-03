96fb9de5f392f4ddafa0e4747876e61a7fbe7260
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-04-11 13:01:25 +0200

    commit-9
    change Item to components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,7 +16,7 @@ function App() {
 					{ id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
 					{ id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
 					{ id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-			];
+			  ];
 	});
 	useEffect(() => {
 		localStorage.setItem('contacts', JSON.stringify(contacts));
